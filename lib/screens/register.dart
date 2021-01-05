e1803e61a5f2c7c43d249f32e56b4733b96a405e
Yogesh Paliyal <paliyalyogesh@gmail.com>
2021-01-05 21:08:14 +0530

    Improved register page

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,13 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sample_app_1/components/gender_horizontal.dart';
+import 'package:sample_app_1/models/user_data.dart';
 import 'package:sample_app_1/screens/dashboard/dashboard.dart';
+import 'package:sample_app_1/utils/sharepref_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterPage extends StatelessWidget {
   static const routeName = "/register";
 
   @override
   Widget build(BuildContext context) {
+    final etName = TextEditingController();
+    final etEmail = TextEditingController();
+    final etPassword = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Register"),
@@ -24,34 +33,65 @@ class RegisterPage extends StatelessWidget {
               child: Form(
                 child: Column(
                   children: [
+                    SizedBox(
+                      height: 20,
+                    ),
                     TextFormField(
+                      controller: etName,
                       textInputAction: TextInputAction.next,
                       keyboardAppearance: Brightness.dark,
                       keyboardType: TextInputType.name,
                       decoration: InputDecoration(
                           labelText: "Name",
-                          icon: Icon(Icons.account_circle_outlined)),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          prefixIcon: Icon(Icons.account_circle_outlined)),
+                    ),
+                    SizedBox(
+                      height: 20,
                     ),
                     TextFormField(
+                      controller: etEmail,
                       textInputAction: TextInputAction.next,
                       keyboardAppearance: Brightness.dark,
                       keyboardType: TextInputType.emailAddress,
                       decoration: InputDecoration(
                           labelText: "Email Address",
-                          icon: Icon(Icons.email_outlined)),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          prefixIcon: Icon(Icons.email_outlined),),
+                    ),
+                    SizedBox(
+                      height: 20,
                     ),
                     TextFormField(
+                      controller: etPassword,
                       textInputAction: TextInputAction.next,
                       keyboardAppearance: Brightness.dark,
                       keyboardType: TextInputType.text,
                       decoration: InputDecoration(
-                          labelText: "Password",
-                          icon: Icon(Icons.lock_outline)),
+                        prefixIcon: Icon(Icons.lock_outline),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.elliptical(5, 5)), ),
+                        labelText: "Password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
                     ),
                     GenderHorizontal(),
+                    SizedBox(
+                      height: 20,
+                    ),
                     RaisedButton(
                         child: Text("Register"),
-                        onPressed: () {
+                        onPressed: () async {
+                          var userData = User.blank();
+                          userData.firstName = "Yogesh";
+                          userData.lastName = "Paliyal";
+                          userData.email = etEmail.text;
+                          var sp = await SharedPreferences.getInstance();
+                          sp.setString(SHARED_PREF_KEYS.LOGIN_DATA,
+                              jsonEncode(userData.toJson()));
                           DashboardPage.start(context);
                         })
                   ],
